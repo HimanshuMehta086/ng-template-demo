7fe25fa5499db7c8b876db93e0c6442ac67419f6
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 15:29:56 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
   counter = 100;
+  header = 'A Nice Day!';
+  body =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
 
   constructor() {}
 
